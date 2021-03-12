dc470a3fde8fb58271dc0fa4f93740c006818068
Sean Ruffolo <portlandlink@gmail.com>
2021-03-12 17:29:12 -0500

    first steps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess a number");
+    println!("Please input your guess:");
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read the line");
+
+    println!("you guessed: {}", guess);
 }
