dec2b3fc533c42211400bf585baeced9c54661bf
Sean Ruffolo <portlandlink@gmail.com>
2021-03-18 12:59:41 -0400

    added libraries

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess a number");
+
+    let secret = rand::thread_rng().gen_range(1,101);
+    println!("The secret number is: {}", secret);
     println!("Please input your guess:");
     let mut guess = String::new();
 
@@ -9,5 +14,14 @@ fn main() {
         .read_line(&mut guess)
         .expect("failed to read the line");
 
+    let guess: u32 = guess.trim().parse().expect("please numbers only!");
+
     println!("you guessed: {}", guess);
+
+    match guess.cmp($secret) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("too big!"),
+        Ordering::Equal => println!("you win"),
+    }
+
 }
